206ebf14f9d721657534659e89fe91b4766215cf
Mabel Li <contact@mabel.li>
2018-11-20 12:10:34 +0000

    New score function to keep scores and print to counts of losses, wins, draws

--- js/engine.js
+++ js/engine.js
@@ -12,7 +12,7 @@ $('button').click(function(event){
   $('#play_you').text(play_you);
   var play_machine = computer_move();
   compare(play_you, play_machine);
-  // score();
+  score(verdict);
 });
 
 // $('#rock').click(function(event) {
@@ -36,6 +36,8 @@ $('button').click(function(event){
 //   compare(play_you, play_machine);
 // });
 
+// **********COMPUTER MOVE FUNCTION**********
+
 function computer_move(){
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
   console.log('move: ' + play_machine);
@@ -43,48 +45,54 @@ function computer_move(){
   play_machine = choices[play_machine];
   $('#play_machine').text(play_machine);
   console.log('move: ' + play_machine);
+  return play_machine;
 }
 
 // **********COMPARE FUNCTION TO REACH VERDICT**********
-function compare(you, machine){
-  var verdict = '';
 
-  if (you == machine) {
+function compare(play_you, play_machine){
+
+  if (play_you == play_machine) {
     verdict = 'draw';
   }
 
-  else if ((you == 'paper') && (machine =='rock')) {
+  else if ((play_you == 'paper') && (play_machine =='rock')) {
     verdict = 'win';
   }
 
-  else if ((you == 'rock') && (machine =='scissors')) {
+  else if ((play_you == 'rock') && (play_machine =='scissors')) {
     verdict = 'win';
   }
 
-  else if ((you == 'scissors') && (machine =='paper')) {
+  else if ((play_you == 'scissors') && (play_machine =='paper')) {
     verdict = 'win';
   }
 
+  else {
+    verdict = 'lose';
+  }
+
+  console.log(verdict);
   $('#verdict').text(verdict);
   played++;
   $('#game_played').text(played);
-}
-
+  }
 
 // **********SWITCH STATEMENT FOR ADDING TO WON, LOST, DRAW, PLAYED**********
-switch (verdict) {
-  case 'win':
-  won++;
-  $('#won').text(won);
-  break;
-case 'lose':
-  lost++;
-  $('#lost').text(lost);
-  break;
-case 'draw':
-  draw++;
-  $('#draw').text(draw);
-  break;
-}
 
-// ?????MAKE THIS PRINT?????
+function score(verdict){
+  switch (verdict) {
+    case 'win':
+    won++;
+    $('#game_won').text(won);
+    break;
+  case 'lose':
+    lost++;
+    $('#game_lost').text(lost);
+    break;
+  case 'draw':
+    draw++;
+    $('#game_draw').text(draw);
+    break;
+  }
+}
