b8da3f80612f9b3ab841f2e0ea4d8a702a9260bd
Mabel Li <contact@mabel.li>
2018-12-05 22:21:01 +0000

    Removed unnecessary variables.

--- js/engine.js
+++ js/engine.js
@@ -2,31 +2,29 @@ var choices = ["rock", "paper", "scissors"];
 var played = 0;
 var won = 0;
 var lost = 0;
-var draw = 0;
-var selected = '';
-var verdict = '';
 
 // Listen for the button clicks
 $('.buttons').click(function(event){
   var play_you = this.getAttribute("value");
-  console.log(play_you);
   $('#play_you').text(play_you);
+  console.log(play_you);
   var play_machine = computer_move();
   compare(play_you, play_machine);
   score(verdict);
 });
 
-function holddown(play_you){
-  if (play_you == 'rock'){
-    $('#rock').css('background-color', 'red');
-  }
-  else if (play_you == 'paper'){
-    $('#paper').css('background-color', 'red');
-  }
-  else {
-    $('#scissors').css('background-color', 'red');
-  }
-}
+// TO DO: CHANGE COLOUR OF SELECTED BUTTON
+// function holddown(play_you){
+//   if (play_you == 'rock'){
+//     $('#rock').css('background-color', 'red');
+//   }
+//   else if (play_you == 'paper'){
+//     $('#paper').css('background-color', 'red');
+//   }
+//   else {
+//     $('#scissors').css('background-color', 'red');
+//   }
+// }
 
 // $('#rock').click(function(event) {
 //   var move_you = 'rock';
@@ -54,7 +52,6 @@ function holddown(play_you){
 function computer_move(){
   var play_machine = Math.floor(Math.random() * (3 - 0)) + 0;
   console.log('move: ' + play_machine);
-
   play_machine = choices[play_machine];
   $('#play_machine').text(play_machine);
   console.log('move: ' + play_machine);
